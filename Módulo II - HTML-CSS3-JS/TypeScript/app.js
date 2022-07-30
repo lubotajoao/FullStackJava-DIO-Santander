0d832c7c7566dc735e2bfc58c15f3ba2d3c2263d
Lubota Ngola Joao <lubota4@gmail.com>
2022-07-30 17:52:36 -0300

    TypeScript: Relação entre tipos de variáveis

--- Módulo II - HTML-CSS3-JS/TypeScript/app.js
+++ Módulo II - HTML-CSS3-JS/TypeScript/app.js
@@ -2,13 +2,19 @@
 var button = document.getElementById('button');
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
-function adicionarNumeros(numero1, numero2) {
-    return numero1 + numero2;
+function adicionarNumeros(numero1, numero2, devePrintar, frase) {
+    var resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
+    return resultado;
 }
+var devePrintar = true;
+var frase = 'O valor é: ';
 if (button) {
     button.addEventListener('click', function () {
         if (input1 && input2) {
-            console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+            console.log(adicionarNumeros(Number(input1.value), Number(input2.value), devePrintar, frase));
         }
     });
 }
