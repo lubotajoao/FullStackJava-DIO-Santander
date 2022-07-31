4a717384a0a1667eb944efb5c58095b661ce22d2
Lubota Ngola Joao <lubota4@gmail.com>
2022-07-31 17:45:58 -0300

    Angular: Criando componentes e construindo o projeto

---
+++ Módulo III - Angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from './course'
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: "Angular: Forms",
+        imageUrl: "",
+        price: 102.99,
+        code: "XPS-3903",
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'November, 2, 2022'
+      },
+      {
+        id: 2,
+        name: "Angular: HTTP",
+        imageUrl: "",
+        price: 46.99,
+        code: "XLS-3903",
+        duration: 80,
+        rating: 4.2,
+        releaseDate: 'December, 4, 2022'
+      }
+    ]
+  }
+}
